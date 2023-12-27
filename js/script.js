f8bec6d7ebe4dfff42d7b4bc5bfb411fe75f00b2
MaciejSzelag <szelag.maciej@gmail.com>
2023-12-27 20:58:59 +0000

    js - menu toggle, section social media added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 const menuWrap = document.querySelector(".menu-lines");
 const menuBox = document.querySelector(".ul-wrap");
-const menuCross = document.querySelector(".lines-wrap")
+const menuCross = document.querySelector(".lines-wrap");
+const navBtns = document.querySelectorAll(".nav-btn");
+let active = true;
+// const socialMediaBtns = document.querySelectorAll(".nav-btn");
 
+//showing menu and changing burger menu to arrow up 
 menuWrap.addEventListener("click", () => {
-    menuBox.classList.toggle("ul-wrap-active");
-    menuCross.classList.toggle("lines-wrap-active")
-
+    if (active) {
+        menuBox.classList.add("ul-wrap-active");
+        menuCross.classList.add("lines-wrap-active");
+        active = !active;
+    } else if (!active) {
+        menuBox.classList.remove("ul-wrap-active");
+        menuCross.classList.remove("lines-wrap-active");
+        active = !active;
+    }
 })
+
+// when it's active remove all active classes
+if (active) {
+    navBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            menuBox.classList.remove("ul-wrap-active");
+            menuCross.classList.remove("lines-wrap-active");
+            active = !active;
+        })
+    })
+}
