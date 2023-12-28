7dde3e952821d4192a8146bab7ba1b6c9d3c450e
MaciejSzelag <szelag.maciej@gmail.com>
2023-12-28 18:03:09 +0000

    about-us.html

--- js/script.js
+++ js/script.js
@@ -30,46 +30,46 @@ if (active) {
 }
 // smoth scrolling
 
-document.addEventListener('DOMContentLoaded', function () {
-    let isScrolling = false;
-    let scrollSpeed = 0;
+// document.addEventListener('DOMContentLoaded', function () {
+//     let isScrolling = false;
+//     let scrollSpeed = 0;
 
-    // Attach the wheel event listener to the body
-    document.body.addEventListener('wheel', handleWheel, { passive: false });
+//     // Attach the wheel event listener to the body
+//     document.body.addEventListener('wheel', handleWheel, { passive: false });
 
-    function handleWheel(event) {
-        // event.preventDefault(); // You can comment out this line to allow default wheel behavior
+//     function handleWheel(event) {
+//         // event.preventDefault(); // You can comment out this line to allow default wheel behavior
 
-        // Adjust the scrolling speed according to your preference
-        const sensitivity = 2; // Adjust this value to control the scrolling speed
-        const delta = event.deltaY || event.detail || -event.wheelDelta;
-        scrollSpeed = delta * sensitivity;
+//         // Adjust the scrolling speed according to your preference
+//         const sensitivity = 2; // Adjust this value to control the scrolling speed
+//         const delta = event.deltaY || event.detail || -event.wheelDelta;
+//         scrollSpeed = delta * sensitivity;
 
-        // Set a flag to avoid overlapping scroll events
-        if (!isScrolling) {
-            isScrolling = true;
-            requestAnimationFrame(decelerate);
-        }
-    }
+//         // Set a flag to avoid overlapping scroll events
+//         if (!isScrolling) {
+//             isScrolling = true;
+//             requestAnimationFrame(decelerate);
+//         }
+//     }
 
-    function decelerate() {
-        // Gradually decrease the scroll speed (deceleration)
-        scrollSpeed *= 0.95;
+//     function decelerate() {
+//         // Gradually decrease the scroll speed (deceleration)
+//         scrollSpeed *= 0.95;
 
-        // Calculate the new scroll position
-        const newScrollTop = window.scrollY + scrollSpeed;
+//         // Calculate the new scroll position
+//         const newScrollTop = window.scrollY + scrollSpeed;
 
-        // Smoothly scroll to the new position with an ease-out timing function
-        window.scrollTo({
-            top: newScrollTop,
-            behavior: 'smooth' // Use 'smooth' for ease-out timing function
-        });
+//         // Smoothly scroll to the new position with an ease-out timing function
+//         window.scrollTo({
+//             top: newScrollTop,
+//             behavior: 'smooth' // Use 'smooth' for ease-out timing function
+//         });
 
-        // Continue decelerating if the scroll speed is significant
-        if (Math.abs(scrollSpeed) > 0.1) {
-            requestAnimationFrame(decelerate);
-        } else {
-            isScrolling = false;
-        }
-    }
-});
+//         // Continue decelerating if the scroll speed is significant
+//         if (Math.abs(scrollSpeed) > 0.1) {
+//             requestAnimationFrame(decelerate);
+//         } else {
+//             isScrolling = false;
+//         }
+//     }
+// });
